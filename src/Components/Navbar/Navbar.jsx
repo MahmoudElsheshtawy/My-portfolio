f06dedfda852ba0a4b4c82d66362ce822641742c
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-26 00:13:22 -1200

    change

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+  <>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div>
+    <div>Navbar</div></>
+  )
+}
+
+export default Navbar
