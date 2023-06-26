112578071a385788adc9c1dd2d3cb13f3853be49
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-26 02:53:41 -1200

    nav

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,47 @@
 import React from 'react'
 import './Navbar.css'
 import logo from '../../photo/logo.png'
+import {FaBars,FaTimes,FaUser} from 'react-icons/fa'
+import {BiLogoGmail} from 'react-icons/bi'
 const Navbar = () => {
   return (
   <>
 <div className="navbar">
     <div className="container">
-        <div className="logo">
-        <img src={logo} alt="" />
-                  </div>
-        <div className="nav-menu"></div>
-        <div className="hamborger"></div>
+      {/* logo */}
+        <div className="logo"><img src={logo} alt=""/></div>
+        {/* contact */}
+        <div className="contact">
+          <div className="number">
+           <FaUser className='icon-contact'/> 
+           <span>01067870535</span>
+
+         </div>
+         <div className="gmail">
+
+          <BiLogoGmail className='icon-contact-1'/>
+          <span>Elsheshtawym54@gmail.com</span>
+
+         </div>
+
+        </div>
+      
+        
+
+       {/* nav-menu */}
+        <div className="nav-menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Skils</li>
+            <li>Work</li>
+            <li>Contact Us</li>
+          </ul>
+        </div>
+        <div className="hamborger">
+          <FaTimes/>
+          <FaBars/>
+        </div>
         
     </div>
 </div>
