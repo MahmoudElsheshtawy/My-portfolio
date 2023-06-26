aa8bcc8d9e8f6a050b7f02bc5db91834a23b7e23
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-26 00:40:53 -1200

    one

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,21 @@
 import React from 'react'
-
+import './Navbar.css'
+import logo from '../../photo/logo.png'
 const Navbar = () => {
   return (
   <>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div>
-    <div>Navbar</div></>
+<div className="navbar">
+    <div className="container">
+        <div className="logo">
+        <img src={logo} alt="" />
+                  </div>
+        <div className="nav-menu"></div>
+        <div className="hamborger"></div>
+        
+    </div>
+</div>
+
+  </>
   )
 }
 
