c105f497ad0727d1f27e9847f949eca9b588b567
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-06-27 00:33:04 -1200

    js

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,23 +2,29 @@ import React from 'react'
 import './Home.css'
 import man from '../../assets/man.png'
 import down from '../../assets/down.png'
+
 const Home = () => {
+
   return (
+
     <div className='home' id='Home'>
+
         <div className="container">
+            {/* left */}
           <div className="left">
             <div className="img-container">
               <img src={man} alt="" />
             </div>
              </div>
+             {/* right */}
           <div className="right"> 
 
           <div className="text-r">
             <h2>Hi there, I'm</h2>
             <h1>Mahmoud Elsheshtawy</h1>
             <h3>Freelance <span>lll</span></h3>
-
           </div>
+          
            <a href='#About'>
             <img src={down} alt="" />
           </a> 
@@ -33,5 +39,19 @@ const Home = () => {
         </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Home
