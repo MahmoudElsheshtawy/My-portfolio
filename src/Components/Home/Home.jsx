7a67840b2b9bd07f80d19cc2f8982ba7ce9ef4f4
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-07-10 04:28:29 -1200

    gg

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,42 +1,79 @@
 import React from 'react'
 import { Typewriter } from 'react-simple-typewriter'
 import video from '../../assets/video.mp4'
+import me from '../../assets/me.jpg'
 import './Home.css'
+import html from '../../photo/html.png'
+import css from '../../photo/css.png'
+import javascript from '../../photo/javascript.png'
+import react from '../../photo/react.png'
+import tillwend from '../../photo/tillwend.png'
+import github from '../../photo/github.png'
+import linkedin from '../../photo/linkedin.png'
 
 const Home = () => {
   return (
     <div className='home' id='Home'>
-      <div className="container">
-        <div className="top-container">
-          <div className="video">
-            <video autoPlay loop muted id='video' >
-           <source src={video} type='video/mp4'/>
+                    <div className="image">
+                    <img src={me} alt="" />
+                    </div>
+                    <div className="container">
+                      <div className="top-container">
+                        <div className="video">
+                          <video autoPlay loop muted id='video' >
+                              <source src={video} type='video/mp4'/>
+                          </video>
+                          
+                        </div>
+                      </div>
 
-            </video>
-          </div>
-        </div>
-        <hr/>
-        <div className="bottom-container">
-        <div className="text-r">
-          <h2>Hi There, I'm;</h2>
-          <h1><i>Mahmoud Elshehtawy.</i></h1>
-          
-          <h3 >Freelance:<span >
-          <Typewriter
-                  words={['Front end developer',]}
-                  loop={70}
-                  cursor
-                  cursorStyle='<<'
-                  typeSpeed={70}
-                  deleteSpeed={50}
-                  delaySpeed={1000}
-     
-           />
-          </span>
-          </h3>
-        </div>
-      </div>
-    </div>
+                      <div className="bottom-container">
+
+                        <div className="text-">
+                             <h2>Hi There, I'm;</h2>
+                               <h1><i>Mahmoud Elshehtawy.</i></h1>
+                        
+                                <h3>Freelance:<span>
+                                <Typewriter
+                                        words={['Front end developer',]}
+                                        loop={70}
+                                        cursor
+                                        cursorStyle='<<'
+                                        typeSpeed={70}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000}
+                          
+                                />
+                                </span>
+                                </h3>
+                        </div>
+                    </div>
+                    {/* <div className="links">
+                      {/* <img src={github} alt="" /> */}
+                      {/* button github */}
+                           
+         
+                      {/* button github */}
+                      {/* <img src={linkedin} alt="" />
+                    </div> */} 
+                    <div className="My-Skils">
+                      <h2>Tech Stack<span>  |</span></h2>
+                     <div className="Skils">
+                      <img src={html} alt="" />
+                      <img src={css} alt="" />
+                      <img src={javascript} alt="" />
+                      <img src={react} alt="" />
+                      <img src={tillwend} alt="" />
+                     </div>
+
+
+                    </div>
+                  </div>
+
+
+
+
+
     </div>
 
   )
