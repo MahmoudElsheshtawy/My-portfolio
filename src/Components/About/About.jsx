a3dd9e04c85b4bdfa40e69cc4e95533f3efd54a5
mahmoud <MahmoudElsheshtawy@users.noreply.github.com>
2023-07-10 04:57:25 -1200

    gg

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='about'>  
+          <div className="contianer">
+            <div className="right">
+         {/* <img src="" alt="" /> */}
+            </div>
+            <div className="left">
+                <h1>ABOUT ME</h1>
+                <h3>A dedicated Front-end Developer based in alexandra, egypt</h3>
+                <p>
+                        As a Junior Front-End Developer, I possess an impressive arsenal of 
+                        skills in HTML, CSS, JavaScript. React.
+                        Tailwind. I excel in designing and maintaining responsive websites 
+                        that offer a smooth user experience. My expertise
+                        lies in crafting dynamic, engaging interfaces through writing cle and
+                        optimized code and utilizing cutting edgeFRONTdevelopment tools
+                        and techniques I am also a team player who thrives in collaborating
+                        with cross-functional teams toproduce outstanding web applications
+                </p>
+            </div>
+          </div>
+        
+        
+        
+        </div>
+  )
+}
+
+export default About
