7196329a335fbca0cd160a77cd46aa4471b75502
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-09-26 14:13:58 +0300

    fantastic

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -11,21 +11,35 @@ const Portfolio = () => {
   <>
   
   <div className="Portfolio" id='Portfolio'>
-    <div className="container" style={{background:'red'}}>
+    <div className="container" style={{background:'#000' ,color:'#fff'}}>
     <h1>Protfolo</h1>
       {/* landeing-page */}
-      <div className="projects">
+      <div className="projects" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" style={{width:'70%',margin:'auto',background:'#000',padding:'1rem',    border:" 1px solid rgb(43 43 43)",marginBottom:'20px'}}>
           {project.map((item)=>(
            
-            <div key={item.id}>
-            <div className='right'>
-                  
-                  <span>{item.name}</span>
-                  <span><img src={item.imgUrl} href="https://vercel.com/mahmoudelsheshtawy/e-commerce-app/4cpLZLmNiAh2QSkzHBQm7Bim1yCA"/></span>
-                 
-                  
-                  </div>
-            <div> </div>
+            <div  key={item.id} style={{display:'flex',}}>
+               <div className="left" style={{display:"flex",flexDirection:'column',flex:'1'}}>
+                      <span style={{fontWeight:'bold',marginBottom:'10px' }} >{item.name}</span>
+                      <span style={{fontSize:'16px',color:'#aaa'}}>{item.information}</span>
+                      <div className="stck-use" style={{marginTop:'30px'}}>
+                        <span style={{ padding:'3px',color:'#000',marginRight:'30px',background: 'rgb(130 130 130)',borderRadius:'5px'}}>React.js</span>
+                        <span style={{ padding:'3px',color:'#000',background: 'rgb(130 130 130)',borderRadius:'5px'}}>vanila-Css</span>
+                        <img></img>
+                        
+                      </div>
+                      <div className="btn" style={{display:'flex',justifyContent:'space-around',alignItems:'center',marginTop:'50px'}} >
+                      <button className="button" ><a style={{color:'#fff'}} href="https://e-commerce-app-git-master-mahmoudelsheshtawy.vercel.app/">Deploy</a></button>
+
+                      <button className="button">
+                         Code
+                      </button>
+                     </div>
+                      </div>
+                      <div className='right ' style={{display:'flex',flex:'1',justifyContent:'flex-end'}}>
+                      <span><img src={item.imgUrl} /></span>
+                      </div>
+                    
+               
             </div>
           ))}
       </div>
