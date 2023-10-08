6574c7a584f7e39eac35bf5f6d72dbde759ba11f
mahmoud elsheshtawy <103583850+MahmoudElsheshtawy@users.noreply.github.com>
2023-10-08 03:04:16 +0300

    finssh div of project

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,9 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
-/* eslint-disable react/jsx-no-undef */
 import './Portfolio.css'
-
-import project from '../../project.json'
-
+import ecomerce from '../../assets/photo/ecomerce.png'
 
 
 const Portfolio = () => {
@@ -11,40 +8,42 @@ const Portfolio = () => {
   <>
   
   <div className="Portfolio" id='Portfolio'>
-    <div className="container" style={{background:'#000' ,color:'#fff'}}>
-    <h1>Protfolo</h1>
-      {/* landeing-page */}
-      <div className="projects" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" style={{width:'70%',margin:'auto',background:'#000',padding:'1rem',    border:" 1px solid rgb(43 43 43)",marginBottom:'20px'}}>
-          {project.map((item)=>(
-           
-            <div  key={item.id} style={{display:'flex',}}>
-               <div className="left" style={{display:"flex",flexDirection:'column',flex:'1'}}>
-                      <span style={{fontWeight:'bold',marginBottom:'10px' }} >{item.name}</span>
-                      <span style={{fontSize:'16px',color:'#aaa'}}>{item.information}</span>
+    
+              {/* commerce website */}
+              <div className="container" style={{background:'#000' ,color:'#fff'}}>
+              {/* <h1>Protfolo</h1> */}
+              <div className="project" style={{display:'flex', border:" 1px solid rgb(43 43 43)" ,padding:'1rem',marginBottom:'20px',flexWrap:'wrap'}}>
+
+                  <div className="left" style={{display:"flex",flexDirection:'column',flex:'1'}}>
+                    <span style={{fontWeight:'bold',marginBottom:'10px' }}>e-commerce website</span> 
+                     <span style={{fontSize:'16px',color:'#aaa'}}>
+                      Im Built a ecommerce website from 
+                      scratch,important topics of react js such as jsx,components, props, state, 
+                      lifecycle of components, conditional rendering, lists, keys, context api.</span>
                       <div className="stck-use" style={{marginTop:'30px'}}>
                         <span style={{ padding:'3px',color:'#000',marginRight:'30px',background: 'rgb(130 130 130)',borderRadius:'5px'}}>React.js</span>
                         <span style={{ padding:'3px',color:'#000',background: 'rgb(130 130 130)',borderRadius:'5px'}}>vanila-Css</span>
                         <img></img>
                         
                       </div>
-                      <div className="btn" style={{display:'flex',justifyContent:'space-around',alignItems:'center',marginTop:'50px'}} >
-                      <button className="button" ><a style={{color:'#fff'}} href="https://e-commerce-app-git-master-mahmoudelsheshtawy.vercel.app/">Deploy</a></button>
+                       <div className="btn" style={{display:'flex',justifyContent:'space-around',alignItems:'center',marginTop:'50px',cursor:'pointer'}} >
+                            <button className="button" ><a style={{color:'#fff'}} href="https://e-commerce-app-git-master-mahmoudelsheshtawy.vercel.app/">Deploy</a></button>
+     
+                               <button className="button">
+                                Code
+                                </button>
+                  </div>
+                     
+                  </div>
+                  <div className="right" style={{display:'flex',flex:'1',justifyContent:'flex-end'}}>
 
-                      <button className="button">
-                         Code
-                      </button>
-                     </div>
-                      </div>
-                      <div className='right ' style={{display:'flex',flex:'1',justifyContent:'flex-end'}}>
-                      <span><img src={item.imgUrl} /></span>
-                      </div>
-                    
-               
-            </div>
-          ))}
-      </div>
- 
-    </div>
+                          <img src={ecomerce} />
+                        </div>
+              </div>
+              </div>
+              {/* scoal-medai-app */}
+             
+                
   </div>
   
   
@@ -63,141 +62,32 @@ export default Portfolio
 
 
 
+/* <div className="container" style={{background:'#000' ,color:'#fff'}}>
+<h1>Protfolo</h1>
+  {/* landeing-page */
+  // <div className="projects" data-aos="fade-up" data-aos-duration="1000" data-aos-anchor-placement="top-bottom" style={{width:'70%',margin:'auto',background:'#000',marginBottom:'20px'}}>
+      // {project.map((item)=>(
+       
+        // <div  key={item.id} style={{display:'flex', border:" 1px solid rgb(43 43 43)" ,padding:'1rem',marginBottom:'20px'}}>
+          //  <div className="left" style={{display:"flex",flexDirection:'column',flex:'1'}}>
+                  /* <span style={{fontWeight:'bold',marginBottom:'10px' }} >{item.name}</span> */
+                  // <span style={{fontSize:'16px',color:'#aaa'}}>{item.information}</span>
+                  // <div className="stck-use" style={{marginTop:'30px'}}>
+                  //   <span style={{ padding:'3px',color:'#000',marginRight:'30px',background: 'rgb(130 130 130)',borderRadius:'5px'}}>React.js</span>
+                  //   <span style={{ padding:'3px',color:'#000',background: 'rgb(130 130 130)',borderRadius:'5px'}}>vanila-Css</span>
+                  //   <img></img>
+                    
+                  // </div>
+                  // <div className="btn" style={{display:'flex',justifyContent:'space-around',alignItems:'center',marginTop:'50px'}} >
+                  // <button className="button" ><a style={{color:'#fff'}} href="https://e-commerce-app-git-master-mahmoudelsheshtawy.vercel.app/">Deploy</a></button>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// imports
-
-
-// import {featuredPortfolio, WebPortfolio, mobilePortfolio,designPortfolio,contentPortfolio,} from "../data";
-// import Portofilolest from "../Portofilolest/Portofilolest"
-// import React, { useState,useEffect } from 'react'
-
-
-
-// in side function
-//    const [selected ,setSelected ]= useState ("featured");
-
-//    const [data, setData] = useState([]);
-
-
-
-//    const List=[
-//         {
-//          id:"featured",
-//          title:"Featured",
-//         },
-//         {
-//          id:"Web",
-//          title:"Web App",
-//         },
-//         {
-//          id:"mobile",
-//          title:"Mobile App",
-//         },
-//         {
-//          id:"design",
-//          title:"Design",
-//         },
-//         {
-//          id:"content",
-//          title:"Content"
-//       },
-//    ];
-// // data
-//    useEffect(() => {
-//       switch (selected) {
-        
-//         case "Featured":
-//           setData(featuredPortfolio);
-//           break;
-//         case "Web":
-//           setData(WebPortfolio);
-//           break;
-//         case "mobile":
-//           setData(mobilePortfolio);
-//           break;
-//         case "design":
-//           setData(designPortfolio);
-//           break;
-//         case "content":
-//           setData(contentPortfolio);
-//           break;
-//         default:
-//           setData(featuredPortfolio);
-//       }
-//     }, [selected]);
-
-//   return (
-//     <div className='Portfolio' id='Portfolio'>
-//               <div className="container">
-//                 <div className="top-container">
-//                   <h1>Protfolo</h1>
-//                     <ul>
-//                       {List.map((item) => (
-//                             <Portofilolest
-//                               title={item.title}
-//                               active={selected === item.id}
-//                               setSelected={setSelected}
-//                               id={item.id}
-//                             />
-//                           ))}
-//                     </ul>
-
-//                 </div>
-
-
-//                 {/* section tow */}
-//             <div className="bottom-container">
-//                   {data.map((data)=>(
-//                       <div className="item">
-//                       <img src={data.img} alt="" />
-//                       <h3>{data.title}</h3>
-//                       </div>
-
-//                   ))}
-    
-//             </div>
-
-//    </div>
-      
-      
-      
-      
-      
-      
-      
-     
-      
-      
-      
-      
-//       </div>
-//   )
+                  // <button className="button">
+                //      Code
+                //   </button>
+                //  </div>
+                //   </div>
+                //   <div className='right ' style={{display:'flex',flex:'1',justifyContent:'flex-end'}}>
+                //   <span><img src={item.imgUrl} /></span>
+                //   </div>
+                
+ 
